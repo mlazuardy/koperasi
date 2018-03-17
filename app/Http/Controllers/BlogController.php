8eb971abca3abb1169ffa0d9e13b5baaaae0f8e1
Michael Lazuardy <shawnmilke55@gmail.com>
2018-03-17 17:54:17 +0700

    fix

--- app/Http/Controllers/BlogController.php
+++ app/Http/Controllers/BlogController.php
@@ -7,6 +7,13 @@ use App\Blog;
 
 class BlogController extends Controller
 {
+
+
+    public function __construct()
+    {
+        $this->middleware('auth');
+    }
+
     public function index()
     {
         $blogs = Blog::get();
